8dba9ca254602ca85f62e39a0ffb822d1ff6bb93
Arique Aguilar <arique1104@gmail.com>
2020-06-08 14:09:36 -0500

    Test is exists with passing code - Enigma

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -3,9 +3,10 @@ require "./lib/enigma"
 
 class EnigmaTest < Minitest::Test
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def test_it_exists
+    enigma = Enigma.new
+    assert_instance_of Enigma, enigma
+  end
 
   #def test_it_has_attributes
   #end
