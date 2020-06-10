d689afcd159941e70b76ebfd797a89d1d4e0ecb4
Arique Aguilar <arique1104@gmail.com>
2020-06-10 13:34:29 -0500

    Confirmation that without hardcoding '12345' the random number does not change across methods.

--- lib/key.rb
+++ lib/key.rb
@@ -1,21 +1,19 @@
 class Key
  attr_reader  :assigned_random_numbers
-  # def initialize
-  #
-  # end
+  def initialize(nums = random_numbers)
+    @assigned_random_numbers = nums
+  end
 
   def random_numbers
-     numbers = rand(99999).to_s.rjust(5, "0")
-     @assigned_random_numbers = numbers
-    require "pry"; binding.pry
+    rand(99999).to_s.rjust(5, "0")
   end
 
   def set_keys
     @assigned_random_numbers
     require "pry"; binding.pry
-    a_thru_d_keys_array = []
-      random_numbers.chars.each_cons(2).each do |num1, num2|
-      require "pry"; binding.pry
+    combined_keys = []
+      @assigned_random_numbers.chars.each_cons(2).each do |num1, num2|
+        require "pry"; binding.pry
       end
   end
 
