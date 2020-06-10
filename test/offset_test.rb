e422ceeef546daa5376034f8d2d55eb454402f7e
Arique Aguilar <arique1104@gmail.com>
2020-06-10 15:29:36 -0500

    Offset_test set to look for lib/offset.rb

---
+++ test/offset_test.rb
@@ -0,0 +1,13 @@
+require "./test/test_helper"
+require "./lib/offset"
+
+class OffsetTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
