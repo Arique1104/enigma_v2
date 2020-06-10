0bdf2d0097c8c24efa7f554601a2bb66bc2dfc0a
Arique Aguilar <arique1104@gmail.com>
2020-06-10 12:21:03 -0500

    Key file require successful

---
+++ test/key_test.rb
@@ -0,0 +1,13 @@
+require "./test/test_helper"
+require "./lib/key"
+
+class KeyTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
